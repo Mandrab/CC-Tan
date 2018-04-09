4f55e4734bcd2a171a66da772734603a53799838
Nicolas Pasolini <nicolas.pasolini@studio.unibo.it>
2018-04-09 15:19:23 +0200

    Modify BallGenerator class

--- src/it/unibo/oop/cctan/model/BallGeneratorImpl.java
+++ src/it/unibo/oop/cctan/model/BallGeneratorImpl.java
@@ -3,6 +3,7 @@ package it.unibo.oop.cctan.model;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 import it.unibo.oop.cctan.model.BallAgent;
 
@@ -18,7 +19,30 @@ public class BallGeneratorImpl extends Thread implements BallGenerator {
 	}
 
 	@Override
-	public void removeBall(BallAgent ball) {
+	public void start() {
+		super.run();
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+			this.createNewBall();
+			try {
+				Thread.sleep(GENERATION_RATIO);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private synchronized void createNewSquare() {
+		BallAgent ball = new BallAgent(this.model, 0/*To Do*/);
+		this.balls.add(ball);
+		ball.run();
+	}
+	
+	@Override
+	public synchronized void removeBall(final BallAgent ball) {
 		if (!this.balls.isEmpty() && ball != null) {
 			ball.terminate();
 			this.balls.remove(ball);
@@ -29,10 +53,4 @@ public class BallGeneratorImpl extends Thread implements BallGenerator {
 	public List<BallAgent> getBallAgents() {
 		return Collections.unmodifiableList(this.balls);
 	}
-
-	@Override
-	public void start() {
-		super.run();
-	}
-
 }
