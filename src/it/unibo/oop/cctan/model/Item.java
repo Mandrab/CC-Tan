405816b6ddb2d2d8a8aae03204167efe949d5ace
Gianmarco Magnani <gianmarco.magnani3@studio.unibo.it>
2018-04-14 00:21:41 +0200

    Improve javadoc style formatting in Item interface

--- src/it/unibo/oop/cctan/model/Item.java
+++ src/it/unibo/oop/cctan/model/Item.java
@@ -9,25 +9,29 @@ public interface Item {
 
     /**
      * Get the model of the MVC application.
-     * @return the model
+     * @return
+     *          the model
      */
     Model getModel();
 
     /**
      * Get the current left-upper bound position of the item.
-     * @return the current position of the item
+     * @return
+     *          the current position of the item
      */
     Point2D getPos();
 
     /**
      * Get the width of the item in proportion to the window width.
-     * @return the width of the item
+     * @return
+     *          the width of the item
      */
     double getWidth();
 
     /**
      * Get the height of the item in proportion to the window height.
-     * @return the height of the item
+     * @return
+     *          the height of the item
      */
     double getHeight();
 }
