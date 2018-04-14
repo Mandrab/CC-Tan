e3eace99f8e9dd9daeed0b70d1c3704e73d42663
Gianmarco Magnani <gianmarco.magnani3@studio.unibo.it>
2018-04-14 02:45:35 +0200

    Adapt *GeneratorImpl to work with builders; add a method to generate random position in SquareGenerator

--- src/it/unibo/oop/cctan/model/BallGeneratorImpl.java
+++ src/it/unibo/oop/cctan/model/BallGeneratorImpl.java
@@ -9,48 +9,52 @@ import it.unibo.oop.cctan.model.BallAgent;
 
 public class BallGeneratorImpl extends Thread implements BallGenerator {
 
-	private static final int GENERATION_RATIO = 200;
-	private List<BallAgent> balls;
-	private final Model model;
-	
-	public BallGeneratorImpl(final Model model) {
-		this.model = model;
-		this.balls = new ArrayList<>();
-	}
+    private static final int GENERATION_RATIO = 200;
+    private final List<BallAgent> balls;
+    private final Model model;
 
-	@Override
-	public void start() {
-		super.run();
-	}
-	
-	@Override
-	public void run() {
-		while (true) {
-			this.createNewBall();
-			try {
-				Thread.sleep(GENERATION_RATIO);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public BallGeneratorImpl(final Model model) {
+        this.model = model;
+        this.balls = new ArrayList<>();
+    }
 
-	private synchronized void createNewBall() {
-		BallAgent ball = new BallAgent(this.model, 0/*To Do*/);
-		this.balls.add(ball);
-		ball.run();
-	}
-	
-	@Override
-	public synchronized void removeBall(final BallAgent ball) {
-		if (!this.balls.isEmpty() && ball != null) {
-			ball.terminate();
-			this.balls.remove(ball);
-		}
-	}
+    @Override
+    public void start() {
+        super.run();
+    }
 
-	@Override
-	public List<BallAgent> getBallAgents() {
-		return Collections.unmodifiableList(this.balls);
-	}
+    @Override
+    public void run() {
+        while (true) {
+            this.createNewBall();
+            try {
+                Thread.sleep(GENERATION_RATIO);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public synchronized void removeBall(final BallAgent ball) {
+        if (!this.balls.isEmpty() && ball != null) {
+            ball.terminate();
+            this.balls.remove(ball);
+        }
+    }
+
+    @Override
+    public List<BallAgent> getBallAgents() {
+        return Collections.unmodifiableList(this.balls);
+    }
+
+    private synchronized void createNewBall() {
+        final BallAgent ball = (BallAgent) new BallAgent.BallBuilder()            //TO DO!
+                                                  .angle(0)
+                                                  .position(null)
+                                                  .model(this.model)
+                                                  .build();
+        this.balls.add(ball);
+        ball.run();
+    }
 }
--- src/it/unibo/oop/cctan/model/SquareGeneratorImpl.java
+++ src/it/unibo/oop/cctan/model/SquareGeneratorImpl.java
@@ -2,53 +2,76 @@ package it.unibo.oop.cctan.model;
 
 import java.util.List;
 import java.util.Random;
+
+import javafx.geometry.Point2D;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class SquareGeneratorImpl extends Thread implements SquareGenerator {
 
-	private static final int GENERATION_RATIO = 2000;
-	private final Model model;
-	private final List<SquareAgent> squares;
+    private static final int GENERATION_RATIO = 2000;
+    private final Model model;
+    private final List<SquareAgent> squares;
 
-	public SquareGeneratorImpl(final Model model) {
-		this.squares = new ArrayList<>();
-		this.model = model;
-	}
+    public SquareGeneratorImpl(final Model model) {
+        this.squares = new ArrayList<>();
+        this.model = model;
+    }
 
-	@Override
-	public void start () {
-		super.start();
-	}
-	
-	@Override
-	public void run() {
-		while (true) {
-			this.createNewSquare();
-			try {
-				Thread.sleep(GENERATION_RATIO);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    @Override
+    public void start() {
+        super.start();
+    }
 
-	private synchronized void createNewSquare() {
-		SquareAgent square = new SquareAgent(this.model, new Random().nextInt(41)+40);
-		this.squares.add(square);
-		square.run();
-	}
+    @Override
+    public void run() {
+        while (true) {
+            this.createNewSquare();
+            try {
+                Thread.sleep(GENERATION_RATIO);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	@Override
-	public synchronized void removeSquare(final SquareAgent square) {
-		if (!this.squares.isEmpty() && square != null) {
-			square.terminate();
-			this.squares.remove(square);
-		}
-	}
+    @Override
+    public synchronized void removeSquare(final SquareAgent square) {
+        if (!this.squares.isEmpty() && square != null) {
+            square.terminate();
+            this.squares.remove(square);
+        }
+    }
 
-	public synchronized List<SquareAgent> getSquareAgents() {
-		return Collections.unmodifiableList(this.squares);
-	}
+    public synchronized List<SquareAgent> getSquareAgents() {
+        return Collections.unmodifiableList(this.squares);
+    }
+
+    private synchronized void createNewSquare() {
+        final Point2D randomPos = randomPosition();
+        final SquareAgent square = (SquareAgent) new SquareAgent.SquareBuilder()
+                                                                .hitPoints(new Random().nextInt(41) + 40)
+                                                                .angle(Math.atan2(-randomPos.getY(), -randomPos.getX())) //Da perfezionare!
+                                                                .position(randomPosition())
+                                                                .model(this.model)
+                                                                .build();
+        this.squares.add(square);
+        square.run();
+    }
+
+    private Point2D randomPosition() {
+        final Random rnd = new Random();
+        final int side = new Random().nextInt(4);
+        if (side == 0 || side == 1) {
+            return (new Point2D(
+                    rnd.nextDouble() * Math.abs(model.getBounds().getX0() - model.getBounds().getX1())
+                            - model.getBounds().getX1(), side == 0 ? model.getBounds().getY1() : model.getBounds().getY0())); //1.2 : -1.2
+        } else {
+            return (new Point2D(side == 2 ? model.getBounds().getX0() : model.getBounds().getX1(),
+                    rnd.nextDouble() * Math.abs(model.getBounds().getY0() - model.getBounds().getY1())
+                            - model.getBounds().getY1())); // -1.2 : 1.2
+        }
+    }
 
 }
