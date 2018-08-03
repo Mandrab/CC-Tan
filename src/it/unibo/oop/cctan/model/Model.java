c60bec85f86f297f7b10fe077c73eab21fb8bb5a
Gianmarco Magnani <gianmarco.magnani3@studio.unibo.it>
2018-08-03 19:12:22 +0200

    Complete Javadoc in Model interface and its implementation

--- src/it/unibo/oop/cctan/model/Model.java
+++ src/it/unibo/oop/cctan/model/Model.java
@@ -2,11 +2,27 @@ package it.unibo.oop.cctan.model;
 
 import it.unibo.oop.cctan.geometry.Boundary;
 
+/**
+ * The model of the MVC pattern, with methods to manage squares and balls.
+ */
 public interface Model extends BallGenerator, SquareGenerator {
 
-    Boundary getBounds();
-
+    /**
+     * Start the Model, launching the two generators (both balls and squares).
+     */
     void start();
 
+    /**
+     * Get the game area boundaries used by the model.
+     * @return
+     *          the current map's boundaries
+     */
+    Boundary getBounds();
+
+    /**
+     * Get the current Shuttle.
+     * @return
+     *          the actual Shuttle item
+     */
     Shuttle getShuttle();
 }
