66084b8db136930a5254e47631886305b63c6fcc
Paolo Baldini <paolo.baldini6@studio.unibo.it>
2018-08-09 13:22:02 +0200

    modified MappableDataAdapter class

--- src/it/unibo/oop/cctan/controller/MappableDataAdapter.java
+++ src/it/unibo/oop/cctan/controller/MappableDataAdapter.java
@@ -1,20 +1,13 @@
 package it.unibo.oop.cctan.controller;
 
 import java.awt.Color;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-
-import org.jhotdraw.geom.Polygon2D;
 
 import it.unibo.oop.cctan.interPackageComunication.MappableData;
 import it.unibo.oop.cctan.interPackageComunication.MappableDataImpl;
-import it.unibo.oop.cctan.model.BallAgent;
 import it.unibo.oop.cctan.model.Model;
 import it.unibo.oop.cctan.model.SquareAgent;
-import javafx.geometry.Point2D;
 
 public class MappableDataAdapter {
 
@@ -26,52 +19,28 @@ public class MappableDataAdapter {
 
     public List<MappableData> getListOfMappableData() {
         //Add all the bullets to the list of mappable data
-        //usare il metodo getShape() per ottenere la shape giusta
-        //System.out.println(model.getBallAgents());
         List<MappableData> l = model.getBulletAgents()
                                     .stream()
                                     .map(ba -> new MappableDataImpl("", 
-                                                                    Color.RED, 
-                                                                    new Ellipse2D.Double(ba.getPos().getX(), 
-                                                                                         ba.getPos().getY(), 
-                                                                                         BallAgent.WIDTH, 
-                                                                                         BallAgent.HEIGHT)))
+                                                                    ba.getColor(), 
+                                                                    ba.getShape()))
                                     .collect(Collectors.toList());
         
         //Add all the squares to the list of mappable data
         l.addAll(model.getSquareAgents()
                       .stream()
                       .map(sa -> new MappableDataImpl("" + ((SquareAgent) sa).getHP(), 
-                                                      calculateColor(((SquareAgent) sa).getHP()), 
-                                                      new Rectangle2D.Double(sa.getPos().getX(), 
-                                                                             sa.getPos().getY(), 
-                                                                             SquareAgent.WIDTH, 
-                                                                             SquareAgent.HEIGHT)))
+                                                      sa.getColor(), 
+                                                      sa.getShape()))
                       .collect(Collectors.toList()));
         
         //Add shuttle to the list of mappable data
         l.add(0, 
               new MappableDataImpl("", 
                                    Color.WHITE,
-                                   new Polygon2D.Double(getAxisPointArray(point -> point.getX()),
-                                                        getAxisPointArray(point -> point.getY()),
-                                                        3)));
+                                   model.getShuttle()
+                                        .getShape()));
         return l;
-    }
-
-    private double[] getAxisPointArray(Function<Point2D, Double> function) {
-        return model.getShuttle()
-                    .getShapePoints()
-                    .stream()
-                    .mapToDouble(point -> function.apply(point).doubleValue())
-                    .toArray();
-    }
-
-    private Color calculateColor(int hp) {
-        /*Color[] cols = new Color[n];
-        for (int i = 0; i < n; i++)
-            cols[i] = Color.getHSBColor((float) i / n, 1, 1);*/
-        return null;
     }
 
 }
