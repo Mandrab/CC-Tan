e2fbf5ceaf238debdb948aedc1481bf1432cb031
Nicolas Pasolini <nicolas.pasolini@studio.unibo.it>
2018-08-13 18:42:15 +0200

    Add setBoundary method in Boundary class

--- src/it/unibo/oop/cctan/geometry/Boundary.java
+++ src/it/unibo/oop/cctan/geometry/Boundary.java
@@ -5,10 +5,10 @@ package it.unibo.oop.cctan.geometry;
  */
 public class Boundary {
 
-    private final double x0;
-    private final double y0;
-    private final double x1;
-    private final double y1;
+    private double x0;
+    private double y0;
+    private double x1;
+    private double y1;
 
     /**
      * @param x0
@@ -24,6 +24,13 @@ public class Boundary {
         this.x0 = x0;
         this.y0 = y0;
         this.x1 = x1;
+        this.y1 = y1;
+    }
+
+    public void setBoundary(final double x0, final double x1, final double y0, final double y1) {
+        this.x0 = x0;
+        this.x1 = x1;
+        this.y0 = y0;
         this.y1 = y1;
     }
 
