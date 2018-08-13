08412964112e78dc114f5cb57f34782e3ee00e40
Paolo Baldini <paolo.baldini6@studio.unibo.it>
2018-08-13 12:39:23 +0200

    resolved wrong git merge

--- src/it/unibo/oop/cctan/view/KeyCommandsListener.java
+++ src/it/unibo/oop/cctan/view/KeyCommandsListener.java
@@ -8,16 +8,7 @@ import it.unibo.oop.cctan.interPackageComunication.Commands;
 import it.unibo.oop.cctan.interPackageComunication.CommandsObserverSourceImpl;
 import it.unibo.oop.cctan.interPackageComunication.GameStatus;
 
-<<<<<<< HEAD
-/**
- * A class created to handle the received keyEvents and send a Command by the newCommand method to the CommandsObservers.
- * @author Sutera Lorenzo
- *
- */
-public class KeyCommandsListener {
-=======
 public class KeyCommandsListener extends CommandsObserverSourceImpl {
->>>>>>> e364e05ebd100dd29b959931c16ad2b6f05f3585
 
     private final View view;
     private KeyListener keyListener;
@@ -28,12 +19,6 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
     private static final int SPACE_KEY_VALUE = 32;
     private static final int ESC_KEY_VALUE = 27;
 
-    /**
-     * The constructor of KeyCommandsListener class.
-     * 
-     *  @param view
-     *            A reference to the view (parents).
-     */
     public KeyCommandsListener(final View view) {
         this.view = view;
         this.reset = false;
@@ -41,42 +26,6 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
         keyListener = new KeyAdapter() {
 
             @Override
-<<<<<<< HEAD
-            public void keyPressed(final KeyEvent e) {
-                List<CommandsObserver> observers = view.getCommandsObserversManager().getCommandsObservers();
-                // int id = e.getID();
-                int keyCode = e.getKeyCode();
-                if (keyCode == KeyCommandsListener.P_KEY_VALUE || keyCode == KeyCommandsListener.SPACE_KEY_VALUE) {
-                    if (actualState.equals(Commands.START) || actualState.equals(Commands.RESUME)) {
-                        for (Iterator<CommandsObserver> i = observers.iterator(); i.hasNext();) {
-                            CommandsObserver c = i.next();
-                            c.newCommand(Commands.PAUSE);
-                        }
-                        actualState = Commands.PAUSE;
-                    } else if (actualState.equals(Commands.PAUSE) && !escPaused) {
-                        for (Iterator<CommandsObserver> i = observers.iterator(); i.hasNext();) {
-                            CommandsObserver c = i.next();
-                            c.newCommand(Commands.RESUME);
-                        }
-                        actualState = Commands.RESUME;
-                    }
-
-                    System.out.println("è stato premuto il tasto p o SPACE per la pausa/resume");
-
-                } else if (keyCode == KeyCommandsListener.ESC_KEY_VALUE) {
-                    if (actualState.equals(Commands.START) || actualState.equals(Commands.RESUME)) {
-                        keyEscPaused(true);
-                        for (Iterator<CommandsObserver> i = observers.iterator(); i.hasNext();) {
-                            CommandsObserver c = i.next();
-                            c.newCommand(Commands.PAUSE);
-                        }
-                        actualState = Commands.PAUSE;
-                     //  avvia schermata ESC
-                        new PauseWindow(view);
-                        System.out.println("esc pressed and conditions are verificated");
-                    }
-                    System.out.println("esc pressed");
-=======
             public void keyPressed(final KeyEvent pressEvent) {
                 int keyCode = pressEvent.getKeyCode();
                 switch (keyCode) {
@@ -95,32 +44,11 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
                         }
                         break;
                     default:
->>>>>>> e364e05ebd100dd29b959931c16ad2b6f05f3585
                 }
             }
         };
     }
 
-<<<<<<< HEAD
-    /**
-     * Send the END command to all the CommandsObservers if internal conditions are respected.
-     * @return
-     *          A boolean value that indicate if the command is sent or not.
-     */
-    public boolean endCommand() {
-        List<CommandsObserver> observers = view.getCommandsObserversManager().getCommandsObservers();
-        if (!actualState.equals(Commands.END)) {
-            for (Iterator<CommandsObserver> i = observers.iterator(); i.hasNext();) {
-                CommandsObserver c = i.next();
-                c.newCommand(Commands.END);
-            }
-            actualState = Commands.END;
-            if (!reset) {
-                // fare partire la schermata finale con score e tutto
-                    new EndWindow(this.view);
-                }
-            reset = false;
-=======
     public KeyListener getKeyListener() {
         return keyListener;
     }
@@ -129,25 +57,11 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
         if (actualState.equals(GameStatus.ENDED)) {
             getCommandsObservers().forEach(co -> co.newCommand(Commands.START));
             actualState = GameStatus.RUNNING;
->>>>>>> e364e05ebd100dd29b959931c16ad2b6f05f3585
             return true;
         }
         return false;
     }
 
-<<<<<<< HEAD
-    /**
-     * Send the START command to all the CommandsObservers if internal conditions are respected.
-     * @return
-     *          A boolean value that indicate if the command is sent or not.
-     */
-    public boolean startCommand() {
-        List<CommandsObserver> observers = view.getCommandsObserversManager().getCommandsObservers();
-        if (actualState.equals(Commands.END)) {
-            for (Iterator<CommandsObserver> i = observers.iterator(); i.hasNext();) {
-                CommandsObserver c = i.next();
-                c.newCommand(Commands.START);
-=======
     public synchronized boolean pauseCommand() {
         if (actualState.equals(GameStatus.RUNNING)) {
             getCommandsObservers().forEach(co -> co.newCommand(Commands.PAUSE));
@@ -173,7 +87,6 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
             if (!reset) {
                 // fare partire la schermata finale con score e tutto
                 new EndWindow(this.view);
->>>>>>> e364e05ebd100dd29b959931c16ad2b6f05f3585
             }
             reset = false;
             return true;
@@ -191,50 +104,6 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
         this.reset = reset;
     }
 
-<<<<<<< HEAD
-    /**
-     * Return the actual state of the class, so the last command sent to the commands Observers.
-     * @return
-     *          a Command that rappresent the actual state of the class.
-     */
-    public Commands getActualState() {
-        return this.actualState;
-    }
-
-    /**
-     * Return the keyListener that have to be applied to the jComponents of the gameWindow.
-     * @return
-     *          a KeyListener that recive the P, SPACE and ESC key events and send commands.
-     */
-    public KeyListener getKeyListener() {
-        return keyListener;
-    }
-
-    /**
-     * Set a variable that will be used as a part of a condition that if it is true the P keyboard pressed event can not send the resume command.
-     * @param escPaused
-     *                  a boolean variable that set the possibility or not for the P button to resume in pause state.
-     */
-    public void keyEscPaused(final boolean escPaused) {
-        this.escPaused = escPaused;
-    }
-
-    /**
-     * Send the RESUME command to all the CommandsObservers if internal conditions are respected.
-     * @return
-     *          A boolean value that indicate if the command is sent or not.
-     */
-    public boolean resumeCommand() {
-        List<CommandsObserver> observers = view.getCommandsObserversManager().getCommandsObservers();
-        if (actualState.equals(Commands.PAUSE)) {
-            for (Iterator<CommandsObserver> i = observers.iterator(); i.hasNext();) {
-                CommandsObserver c = i.next();
-                c.newCommand(Commands.RESUME);
-            }
-            actualState = Commands.RESUME;
-            System.out.println(getActualState());
-            return true;
-=======
     public synchronized void forceCommand(final Commands command) {
         switch (command) {
             case START:
@@ -250,7 +119,6 @@ public class KeyCommandsListener extends CommandsObserverSourceImpl {
                 endCommand();
                 break;
             default:
->>>>>>> e364e05ebd100dd29b959931c16ad2b6f05f3585
         }
     }
 
--- src/it/unibo/oop/cctan/view/SizeObserverSourceImpl.java
+++ src/it/unibo/oop/cctan/view/SizeObserverSourceImpl.java
@@ -2,36 +2,16 @@ package it.unibo.oop.cctan.view;
 
 import java.awt.Dimension;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-<<<<<<< HEAD:src/it/unibo/oop/cctan/view/SizeObserversManager.java
-/**
- * A class created to handle the sizeObservers list.
- * @author Sutera Lorenzo
- */
-public class SizeObserversManager implements SizeObserverSource {
-
-    private List<SizeObserver> sizesObservers;
-
-    /**
-     * The constructor of SizeObserverManager class.
-     */
-    public SizeObserversManager() {
-=======
 import org.apache.commons.lang3.tuple.Pair;
 
 public abstract class SizeObserverSourceImpl implements SizeObserverSource {
 
     private List<SizeObserver> sizesObservers;
 
-<<<<<<< HEAD:src/it/unibo/oop/cctan/view/SizeObserversSourceImpl.java
-    public SizeObserversSourceImpl() {
->>>>>>> 6dd02bf9f29d4edf3b1379ff23c5b0eb0680cb17:src/it/unibo/oop/cctan/view/SizeObserversSourceImpl.java
-=======
     public SizeObserverSourceImpl() {
->>>>>>> e364e05ebd100dd29b959931c16ad2b6f05f3585:src/it/unibo/oop/cctan/view/SizeObserverSourceImpl.java
         sizesObservers = new ArrayList<SizeObserver>();
     }
 
